61cd619b6b9364bb6209b7ec168b42c4a6241ba2
Shubhankar Agarwal <sagarwal@uber.com>
2018-11-20 23:52:29 -0500

    Added simulator setup script

--- simulation-essentials/simulator_install.sh
+++ simulation-essentials/simulator_install.sh
@@ -1,24 +1,25 @@
-mkdir -p catkin_ws/src
-cd catkin_ws
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws
 catkin_make
 cd src
 git clone https://github.com/ros-drivers/video_stream_opencv.git
 cd ..
 catkin_make
 
-
-### Simulator 
-cd src 
+### Simulator
+cd src
 git clone https://github.com/uuvsimulator/uuv_simulator.git
 
+cd ~/catkin_ws
 source /usr/share/gazebo-7/setup.sh
 source /opt/ros/kinetic/setup.bash
-source /devel/setup.bash
+source devel/setup.bash
 
 source ~/.bashrc
 
 cd ~/catkin_ws
 rosdep install --from-paths src --ignore-src --rosdistro=kinetic -y
+# rosdep install --from-paths src --ignore-src --rosdistro=indigo -y --skip-keys "gazebo gazebo_msgs gazebo_plugins gazebo_ros gazebo_ros_control gazebo_ros_pkgs"
 
 cd ~/catkin_ws
 catkin_make install
@@ -30,7 +31,14 @@ cd ..
 cd robosub-worlds-models
 cp -r * ~/catkin_ws/src/uuv_simulator/uuv_descriptions/world_models
 
+cd ..
+cp velocity_control.launch  ~/catkin_ws/src/uuv_simulator/uuv_control/uuv_control_cascaded_pids/launch/
+
 cd ~/catkin_ws/src/
 mv -f ~/tartan-sub/ .
 cd ..
 catkin_make install
+
+source /usr/share/gazebo-7/setup.sh
+source /opt/ros/kinetic/setup.bash
+source devel/setup.bash
