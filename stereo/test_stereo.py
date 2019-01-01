f2f23557c4a10be8917a24f155edf2d34567d8e7
Alvinosaur <ashek@andrew.cmu.edu>
2018-12-31 20:13:38 -0800

    Added two test images from simulation's stereo cameras Added basic scripts for stereo, not good disparity map

--- stereo/test_stereo.py
+++ stereo/test_stereo.py
@@ -5,25 +5,56 @@ import os
 
 imL_path = 'left_image.png'
 imR_path = 'right_image.png'
-
-
-# im1_path = 'Flowers-perfect/Tsukuba_L.png'
-# im2_path = 'Flowers-perfect/Tsukuba_R.png'
+# SGBM Parameters -----------------
 assert(os.path.isfile(imL_path))
 assert(os.path.isfile(imR_path))
 
-imgL = cv2.imread(imL_path, 0)
-imgR = cv2.imread(imR_path, 0)
-# print(type(imgL))
-# imgL_frame=cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
-# CV_<number_of_bits_per_channel><Unsigned/Signed/Float>C<number_of_channels>
-# CV_8UC1 is a single-channel matrix oc uint8_t
-# imgR_frame=cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+imgL = cv2.imread(imL_path)
+imgR = cv2.imread(imR_path)
 
-stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-disparity = stereo.compute(imgR, imgL)
-plt.imshow(imgR)
-plt.imshow(imgL)
-plt.imshow(disparity, 'gray')
+window_size = 3                     # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+lmbda = 80000
+sigma = 1.2
+visual_multiplier = 1.0
+
+left_matcher = cv2.StereoSGBM_create(
+    minDisparity=60,
+    numDisparities=192,             # max_disp has to be dividable by 16 f. E. HH 192, 256
+    blockSize=7,
+    P1=8 * 3 * window_size ** 2,    # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+    P2=32 * 3 * window_size ** 2,
+    disp12MaxDiff=1,
+    uniquenessRatio=5,
+    speckleWindowSize=0,
+    speckleRange=1,
+    preFilterCap=63,
+    mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
+)
+right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
+ 
+wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
+wls_filter.setLambda(lmbda)
+wls_filter.setSigmaColor(sigma)
+
+displ = left_matcher.compute(imgL, imgR)  # .astype(np.float32)/16
+dispr = right_matcher.compute(imgR, imgL)  # .astype(np.float32)/16
+displ = np.int16(displ)
+dispr = np.int16(dispr)
+
+filteredImg = wls_filter.filter(displ, imgL, None, dispr)  # important to put "imgL" here!!!
+filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX);
+filteredImg = np.uint8(filteredImg)
+# im1_path = 'Flowers-perfect/Tsukuba_L.png'
+# im2_path = 'Flowers-perfect/Tsukuba_R.png'
+
+# disparity = stereo.compute(imgL, imgR)
+# norm_image = cv2.normalize(disparity, None, alpha = 0, beta = 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+cv2.imshow("Left", imgR)
+cv2.imshow("Right", imgL)
+# cv2.imshow('Disparity', disparity)
+# cv2.imshow('norm_image', norm_image)
+cv2.imshow("filteredImg", filteredImg)
+cv2.waitKey()
+cv2.destroyAllWindows()
 # plt.imshow(imgL, 'gray')
-plt.show()
+# plt.show()
