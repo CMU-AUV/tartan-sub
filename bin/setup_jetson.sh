6809d32af22b9e6c92b780f0c125c8c893292dd5
tartanauv <Tartan AUV>
2019-06-30 13:36:06 -0400

    stereo data collection script

---
+++ bin/setup_jetson.sh
@@ -0,0 +1,2 @@
+sudo chmod 666 /dev/ttyACM0
+sudo bash -c 'echo -1 > /sys/module/usbcore/parameters/autosuspend'
--- src/utils/collect_stereo_data.py
+++ src/utils/collect_stereo_data.py
@@ -5,31 +5,25 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 
-CAMERA_WIDTH = 1280
-CAMERA_HEIGHT = 720
+CAMERA_WIDTH = 640
+CAMERA_HEIGHT = 480
 
 BRIDGE = CvBridge()
 
-# TODO: Use more stable identifiers
-left = cv2.VideoCapture(1)
-right = cv2.VideoCapture(2)
+def set_cam_params(cam_num):
+    cap = cv2.VideoCapture(cam_num)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
+    # cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+    return cap
 
-# Increase the resolution
-left.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
-left.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
-right.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
-right.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
 
-# Use MJPEG to avoid overloading the USB 2.0 bus at this resolution
-left.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
-right.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
-
-left_image_pub = rospy.Publisher("/sj_camera/left_image_raw", Image)
-right_image_pub = rospy.Publisher("/sj_camera/right_image_raw", Image)
+left_image_pub = rospy.Publisher("/sj_camera/left_image_raw", Image, queue_size=1)
+right_image_pub = rospy.Publisher("/sj_camera/right_image_raw", Image, queue_size=1)
 
 # The distortion in the left and right edges prevents a good calibration, so
 # discard the edges
-CROP_WIDTH = 960
+CROP_WIDTH = CAMERA_WIDTH - 20
 def cropHorizontal(image):
     return image[:,
             int((CAMERA_WIDTH-CROP_WIDTH)/2):
@@ -37,30 +31,68 @@ def cropHorizontal(image):
 
 frameId = 0
 
+def get_video_writer(fname, fps, width, height):
+    gst_str = ("appsrc ! videoconvert ! omxh264enc ! mpegtsmux ! "
+               "filesink location={}.ts ").format(fname)
+    return cv2.VideoWriter(gst_str, cv2.CAP_GSTREAMER, 0, fps, (width, height))
+
+# left_video = get_video_writer('left_video', 5, CAMERA_WIDTH, CAMERA_HEIGHT)
+# right_video = get_video_writer('right_video', 5, CAMERA_WIDTH, CAMERA_HEIGHT)
+
+rospy.init_node('camera_publisher', anonymous=True)
+
+left = set_cam_params(0)
+right = set_cam_params(1)
+# down = set_cam_params(2)
+
+
 # Grab both frames first, then retrieve to minimize latency between cameras
 while(True):
-    if not (left.grab()): #  and right.grab()):
+
+    if not (left.grab()):
         print("No more frames")
         break
 
     _, leftFrame = left.retrieve()
-    leftFrame = cropHorizontal(leftFrame)
+    leftFrame = cv2.resize(leftFrame, None,fx=0.5, fy=0.66, interpolation = cv2.INTER_AREA)
+
+    if not (right.grab()):
+        print("No more frames")
+        break
     _, rightFrame = right.retrieve()
-    rightFrame = cropHorizontal(rightFrame)
-    
-    left_image_pub.publish(BRIDGE.cv2_to_imgmsg(cv_image, "bgr8"))
-    right_image_pub.publish(BRIDGE.cv2_to_imgmsg(cv_image, "bgr8"))
+    rightFrame = cv2.resize(rightFrame, None,fx=0.5, fy=0.66, interpolation = cv2.INTER_AREA)
+
+    print(leftFrame.shape)
+
+    # if not (down.grab()):
+    #     print("No more frames")
+    #     break
+
+    # _, leftFrame = left.retrieve()
+    # _, rightFrame = right.retrieve()
+    # _, downFrame = down.retrieve()
+
+    # left_video.write(leftFrame)
+    # right_video.write(rightFrame)
 
     if frameId % 10 == 0:
         cv2.imshow('left', leftFrame)
         cv2.imshow('right', rightFrame)
+        # cv2.imshow('down', downFrame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    left_image_pub.publish(BRIDGE.cv2_to_imgmsg(leftFrame, "bgr8"))
+    right_image_pub.publish(BRIDGE.cv2_to_imgmsg(rightFrame, "bgr8"))
+
+    if cv2.waitKey(5) & 0xFF == ord('q'):
         break
 
     frameId += 1
 
 left.release()
 right.release()
+# down.release()
+
+# left_video.release()
+# right_video.release()
 
 cv2.destroyAllWindows()
