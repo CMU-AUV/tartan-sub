385addcd01494aadceb938b9c6927be331468b45
Shubhankar Agarwal <sagrwl11@illinois.edu>
2019-07-29 20:18:22 -0700

    Added Barrel Roll

---
+++ src/robosub2019/barrel_roll.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import rospy
+
+from task import Task
+from geometry_msgs.msg import Twist
+import time
+
+class BarrelRoll(Task):
+    def __init__(self, sub_controller):
+        self.mover = sub_controller.mover
+        seff.start_time = time.time()
+
+    def roll(self):
+		msg = Twist()
+
+		msg.linear.x = 0.3
+		msg.angular.x = 0.2
+
+		self.mover.publish(msg)
+
+    def execute(self):
+		while(not rospy.is_shutdown() && (time.time() - self.start_time < 15.0)):
+            self.roll()
---
+++ src/robosub2019/run_barrel_roll.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import rospy
+
+from motion_utilities import Mover
+from gate import Gate
+from vamp_visual_servo import VampVisualServoing
+from barrel_roll import BarrelRoll
+from config import ConfigMap, SimConfig, SubConfig
+from armer import Armer
+
+
+class SubController(object):
+    def __init__(self, run_config):
+        self.mover = Mover(run_config)
+        self.armer = Armer(run_config)
+
+
+if __name__ == "__main__":
+    rospy.init_node('tartan_19_controller', anonymous=True)
+
+    run_config = ConfigMap['Sub']
+    sub_controller = SubController(run_config)
+
+    roll = BarrelRoll(sub_controller)
+
+    print("Arming")
+    sub_controller.armer.arm()
+
+    sub_controller.mover.dive(3.0, -0.2)
+
+    roll.execute()
+
+    sub_controller.armer.disarm()
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main(sys.argv)
