713f53653fc139d2e45625ca697bb40818976f67
Joel Manning <stopwatchj@gmail.com>
2019-09-22 12:33:16 -0400

    Update simulator_install.sh
    ROS Kinetic -> ROS Melodic
    Gazebo 7 -> Gazebo 9
    uuv_gazebo_worlds/world_models -> uuv_gazebo_worlds/models

--- simulation-essentials/simulator_install.sh
+++ simulation-essentials/simulator_install.sh
@@ -11,14 +11,14 @@ cd src
 git clone https://github.com/uuvsimulator/uuv_simulator.git
 
 cd ~/catkin_ws
-source /usr/share/gazebo-7/setup.sh
-source /opt/ros/kinetic/setup.bash
+source /usr/share/gazebo-9/setup.sh
+source /opt/ros/melodic/setup.bash
 source devel/setup.bash
 
 source ~/.bashrc
 
 cd ~/catkin_ws
-rosdep install --from-paths src --ignore-src --rosdistro=kinetic -y
+rosdep install --from-paths src --ignore-src --rosdistro=melodic -y
 # rosdep install --from-paths src --ignore-src --rosdistro=indigo -y --skip-keys "gazebo gazebo_msgs gazebo_plugins gazebo_ros gazebo_ros_control gazebo_ros_pkgs"
 
 cd ~/catkin_ws
@@ -29,7 +29,7 @@ cp * ~/catkin_ws/src/uuv_simulator/uuv_gazebo_worlds/worlds/
 
 cd ..
 cd robosub-worlds-models
-cp -r * ~/catkin_ws/src/uuv_simulator/uuv_gazebo_worlds/world_models/
+cp -r * ~/catkin_ws/src/uuv_simulator/uuv_gazebo_worlds/models/
 
 cd ..
 cp velocity_control.launch  ~/catkin_ws/src/uuv_simulator/uuv_control/uuv_control_cascaded_pids/launch/
@@ -39,6 +39,6 @@ mv -f ~/tartan-sub/ .
 cd ..
 catkin_make install
 
-source /usr/share/gazebo-7/setup.sh
-source /opt/ros/kinetic/setup.bash
+source /usr/share/gazebo-9/setup.sh
+source /opt/ros/melodic/setup.bash
 source devel/setup.bash
